59027416e84d629940ebf9b4755f131b71e102af
Nando San Miguel <contact@namoku.xyz>
2022-01-25 19:09:04 -0600

    Add button component
    Co-authored-by: danthek <abraham8507@gmail.com>
    Co-authored-by: AangelF <Aangel.Flores.R@gmail.com>
    Co-authored-by: adrianagh13 <gomezhinojosa.adriana@gmail.com>

---
+++ src/components/atoms/index.js
@@ -0,0 +1,1 @@
+export {GeneralButton} from './GeneralButton.js';
---
+++ src/theme/default.js
@@ -0,0 +1,9 @@
+const Color = {
+  primary: '#26cbdf',
+  secondary: '#017897',
+  hover: '#94e9f1',
+  background: '#ffffff',
+  text: '#888888',
+};
+
+export default Color;
