107b640654f53d539190c8400150e7f534ef7da6
AangelF <Aangel.Flores.R@gmail.com>
2022-01-31 23:35:23 -0600

    Register screen finished

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,6 +1,63 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
+import { GeneralButton, GeneralInput, GeneralLink, GeneralText } from '../components/atoms';
+import { GeneralHeader } from '../components/molecules/index'
 
-export const Register = () => {
-  return <Text>Register</Text>;
+export const Register = (props) => {
+  return (
+  <>
+    <GeneralHeader/>
+      <View style={styles.registro}>
+        <GeneralText 
+          size={"h1"}
+          color={"secondary"}
+          title={"Regístrate"}
+          weight
+        />
+      </View>
+      <View style={styles.center}>
+        <GeneralInput
+          title={"Nombre Completo"}
+          placeholder={"Tu nombre"}
+        />
+        <GeneralInput
+          title={"Correo"}
+          placeholder={"Tu correo"}
+        />
+        <GeneralInput
+          title={"Contraseña"}
+          placeholder={"Tu contraseña"}
+          secret
+        />
+        <GeneralButton
+          title={"Registrarse"}
+          color={"primary"}
+          size={340}
+        />
+        <Text>O si gustas</Text>
+        <GeneralButton
+          title={"Registrate con Facebook"}
+          color={"facebook"}
+          size={340}
+        />
+        <GeneralLink
+          title={"¿Ya eres usuario? Inicia sesión"}
+          size={"h4"}
+          color={"secondary"}
+          action={() => props.navigation.navigate('Login')}
+        />
+      </View>
+  </>
+  )
 };
+
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  registro: {
+    marginLeft: 10,
+  },
+});
