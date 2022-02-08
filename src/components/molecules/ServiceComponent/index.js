dad45110872fb399202c64a23b35361b38e0b842
danthek <abraham8507@gmail.com>
2022-02-08 16:15:18 -0600

    service component folder created

---
+++ src/components/molecules/ServiceComponent/index.js
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export const ServiceComponent = () => {
+  return (
+    <View>
+      <Text>HOla</Text>
+    </View>
+  );
+};
