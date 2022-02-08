4bbef483bc616a9d9fb09019737de56ca4fd8e2c
adrianagh13 <gomezhinojosa.adriana@gmail.com>
2022-02-08 10:59:18 -0600

    StarRating component finished The component can receive props

--- src/components/atoms/StarRating/index.js
+++ src/components/atoms/StarRating/index.js
@@ -1,9 +1,16 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Rating} from 'react-native-elements';
 
-export const StarRating = () => {
+export const StarRating = ({quantity, readBoolean, startValue}) => {
   const ratingCompleted = rating => {
     console.log('Rating is ' + rating);
   };
-  return <Rating readonly={false} onFinishRating={ratingCompleted} />;
+  return (
+    <Rating
+      ratingCount={quantity}
+      readonly={readBoolean}
+      onFinishRating={ratingCompleted}
+      startingValue={startValue}
+    />
+  );
 };
--- src/screens/Recruiter/ServiceScore.js
+++ src/screens/Recruiter/ServiceScore.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import { StarRating } from '../../components/atoms';
+import {StarRating} from '../../components/atoms';
 
 export const ServiceScore = () => {
   return (
     <>
       <SafeAreaView />
-      <StarRating />
+      <StarRating quantity={1} readBoolean={true} startValue={1} />
     </>
   );
 };
