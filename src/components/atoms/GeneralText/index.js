84eb5e17c6d9ae8f3e418202aac193dcec885325
Nando San Miguel <contact@namoku.xyz>
2022-02-09 17:51:24 -0600

    Add icon to generaltext
    Co-authored-by: adrianagh13 <gomezhinojosa.adriana@gmail.com>

--- src/components/atoms/GeneralText/index.js
+++ src/components/atoms/GeneralText/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import {AppText} from './styled';
+import {AppText, TextContainer} from './styled';
 import {Color, Size} from '../../../theme/default';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-export const GeneralText = ({size, color, title, weight}) => {
+export const GeneralText = ({size, color, title, weight, icon}) => {
   color ? (color = Color[color]) : (color = Color.text);
+  const sizeIcon = {
+    h1: 24,
+    h2: 22,
+    h3: 20,
+    h4: 18,
+    h5: 16,
+    h6: 14,
+  };
 
   return (
-    <AppText size={Size[size]} color={color} weight={weight}>
-      {title}
-    </AppText>
+    <TextContainer>
+      {icon ? <Icon name={icon} size={sizeIcon[size]} color /> : undefined}
+      <AppText size={Size[size]} color={color} weight={weight}>
+        {icon ? ' ' : undefined}
+        {title}
+      </AppText>
+    </TextContainer>
   );
 };
 
--- src/components/atoms/GeneralText/styled.js
+++ src/components/atoms/GeneralText/styled.js
@@ -4,5 +4,9 @@ export const AppText = styled.Text`
   font-size: ${props => props.size};
   color: ${props => props.color};
   font-weight: ${props => (props.weight ? 'bold' : 'normal')};
-  align-self: center;
+`;
+
+export const TextContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
 `;
