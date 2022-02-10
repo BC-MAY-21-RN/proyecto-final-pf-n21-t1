5bd37f619db5872d490fc227245f4f27093a4656
AangelF <Aangel.Flores.R@gmail.com>
2022-02-10 14:37:06 -0600

    Add UpcomingServiceComponent and screen UpcomingServices

---
+++ src/components/molecules/UpcomingServicesComponent/upcomingServiceData.js
@@ -0,0 +1,26 @@
+const upcomingServiceData = () => {
+  return [
+    {
+      title:'Fecha:',
+      size:'h6',
+      color:'text'
+    },
+    {
+      title:'Horario:',
+      size:'h6',
+      color:'text'
+    },
+    {
+      title:'Proveedor de Servicio:',
+      size:'h6',
+      color:'text'
+    },
+    {
+      title:'Cliente:',
+      size:'h6',
+      color:'text'
+    },
+  ];
+};
+
+export default upcomingServiceData;
