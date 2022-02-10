f8792ad001f555530c0acad16522b479a41baea8
adrianagh13 <gomezhinojosa.adriana@gmail.com>
2022-02-10 16:20:52 -0600

    Regenerated the GeneralText in Card Added the icon prop to general text

--- src/components/molecules/ProviderCard/index.js
+++ src/components/molecules/ProviderCard/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
-import Icon from 'react-native-vector-icons/Ionicons';
 import {GeneralText, StarRating} from '../../atoms';
-import {
-  AvatarImg,
-  CardOpacity,
-  ImgContainer,
-  InfoContainer,
-  NumberRow,
-} from './styled';
+import {AvatarImg, CardOpacity, ImgContainer, InfoContainer} from './styled';
 
 export const ProviderCard = ({
   name,
@@ -27,10 +20,12 @@ export const ProviderCard = ({
       </ImgContainer>
       <InfoContainer>
         <GeneralText title={name} size="h3" color="secondary" weight />
-        <NumberRow>
-          <Icon name="call-outline" size={20} />
-          <GeneralText title={number} size="h4" color="text" />
-        </NumberRow>
+        <GeneralText
+          title={number}
+          size="h4"
+          color="text"
+          icon="call-outline"
+        />
         <GeneralText title={zone} size="h4" color="text" />
         <StarRating
           quantity={5}
