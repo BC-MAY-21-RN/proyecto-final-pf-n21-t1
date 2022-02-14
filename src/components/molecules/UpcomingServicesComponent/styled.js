16fcd73b431b5017c997cc38de0b1155f9dd44a2
danthek <abraham8507@gmail.com>
2022-02-14 15:29:14 -0600

    update to master

---
+++ src/components/molecules/UpcomingServicesComponent/styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const GroupRow = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 3%;
+`;
+export const GroupColumn = styled.View`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  margin-bottom: 3%;
+`;
