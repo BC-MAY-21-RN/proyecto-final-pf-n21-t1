48ea1b1d8f02e0fc0681e5b2eac21700bf1eff99
adrianagh13 <gomezhinojosa.adriana@gmail.com>
2022-02-22 13:07:16 -0600

    Refactor styles Used styled components instead of style sheets

--- src/components/molecules/MenuModal/index.js
+++ src/components/molecules/MenuModal/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
-import {
-  Modal,
-  View,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-  StyleSheet,
-} from 'react-native';
-import {} from './styled';
+import {View, TouchableWithoutFeedback} from 'react-native';
+import {Container, ContainerScreen, LinkStyle} from './styled';
 import {GeneralLink, GeneralModal, GeneralText} from '../../atoms';
 import HomeIcon from '../../../assets/icons/Home';
 import Icon from 'react-native-vector-icons/Ionicons';
@@ -14,10 +8,10 @@ import {Color} from '../../../theme/default';
 
 const LinkIcon = ({icon, title}) => {
   return (
-    <View style={style.inicio}>
+    <LinkStyle>
       <Icon name={icon} size={22} color={Color.secondary} />
       <GeneralLink title={'  ' + title} size="h5" color="secondary" />
-    </View>
+    </LinkStyle>
   );
 };
 
@@ -27,55 +21,24 @@ export const MenuModal = ({navigation, modalVisible, setModalVisible}) => {
       animation="fade"
       modalVisible={modalVisible}
       setModalVisible={setModalVisible}>
-      <TouchableOpacity
-        style={style.containerScreen}
+      <ContainerScreen
         activeOpacity={1}
         onPressOut={() => setModalVisible(false)}>
-        <View style={style.container}>
+        <Container>
           <TouchableWithoutFeedback>
             <View>
-              <TouchableOpacity style={style.inicio}>
+              <LinkStyle>
                 <HomeIcon width={22} colorIcon="secondary" />
                 <GeneralText title="  Inicio" size="h4" color="secondary" />
-              </TouchableOpacity>
+              </LinkStyle>
               <LinkIcon title="Contratar servicios" icon="hammer" />
               <LinkIcon title="Servicios contratados" icon="cog" />
               <LinkIcon title="Editar dirección" icon="create" />
               <LinkIcon title="Cerrar sesión" icon="log-out" />
             </View>
           </TouchableWithoutFeedback>
-        </View>
-      </TouchableOpacity>
+        </Container>
+      </ContainerScreen>
     </GeneralModal>
   );
 };
-
-const style = StyleSheet.create({
-  inicio: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-    alignSelf: 'flex-start',
-  },
-  containerScreen: {
-    width: '100%',
-    height: '100%',
-    position: 'absolute',
-  },
-  container: {
-    backgroundColor: 'white',
-    width: 200,
-    height: 100,
-    marginTop: '18.1%',
-    position: 'absolute',
-    right: 0,
-    marginRight: '4%',
-    boxShadow: '0px 8px 10px rgba(0, 0, 0, 0.2)',
-    borderBottomRightRadius: 15,
-    borderBottomLeftRadius: 15,
-    elevation: 5,
-    alignItems: 'center',
-    flexDirection: 'column',
-    justifyContent: 'space-evenly',
-  },
-});
--- src/components/molecules/MenuModal/styled.js
+++ src/components/molecules/MenuModal/styled.js
@@ -1,1 +1,32 @@
 import styled from 'styled-components';
+import { Color } from '../../../theme/default';
+
+export const LinkStyle = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+  align-self: flex-start;
+`;
+
+export const ContainerScreen = styled.TouchableOpacity`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+`;
+
+export const Container = styled.TouchableOpacity`
+  background-color: ${Color.background};
+  width: 200px;
+  height: 100px;
+  margin-top: 18.1%;
+  position: absolute;
+  right: 0;
+  margin-right: 4%;
+  box-shadow: 0px 8px 10px rgba(0, 0, 0, 0.2);
+  border-bottom-right-radius: 15px;
+  border-bottom-left-radius: 15px;
+  elevation: 5,
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-evenly;
+`;
