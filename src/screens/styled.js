98a4e3526944f7b317fc2f4c81ef9e7ec3a28918
Nando San Miguel <contact@namoku.xyz>
2022-02-22 15:35:16 -0600

    Fix codeclimate issue

--- src/screens/styled.js
+++ src/screens/styled.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+const center = 'align-items: center; justify-content: center;';
+const centerEnd = 'align-items: center; justify-content: flex-end;';
 
 export const PickerWrapper = styled.View`
   flex-direction: row;
@@ -8,20 +10,17 @@ export const CenterView = styled.View`
   height: 10%;
   align-self: center;
   width: 90%;
-  align-items: center;
-  justify-content: center;
+  ${center}
 `;
 
 export const MarginView = styled.View`
-  align-items: center;
-  justify-content: flex-end;
+  ${centerEnd}
   margin-horizontal: 10px;
-  height:${props => (props.height ? props.height : '8%')};
+  height: ${({height}) => (height ? height : '8%')};
 `;
 
 export const InputView = styled.View`
   height: 22%;
-  align-items: center;
-  justify-content: center;
+  ${center}
 `
 
