ef7fe9809aafdf96562efeba1f5858536052fdf9
Namoku <contact@namoku.xyz>
2022-02-28 12:28:01 -0600

    Add movable marker

--- src/screens/Recruiter/MapDirection.js
+++ src/screens/Recruiter/MapDirection.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, View, Alert, PermissionsAndroid} from 'react-native';
 import {
   Container,
   ContainerWhite,
@@ -7,8 +7,45 @@ import {
   GeneralText,
 } from '../../components/atoms';
 import {GeneralHeader} from '../../components/molecules';
+import MapView, {Marker} from 'react-native-maps';
+import Geolocation from 'react-native-geolocation-service';
+
+const requestPermission = async setPermission => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) setPermission(true);
+    else {
+      Alert.alert('Error', 'Debes de darle permisos de ubicación.', [
+        {text: 'Ok', onPress: () => requestPermission(setPermission)},
+      ]);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const ScreenInfo = () => {
+  const [isPermission, setPermission] = useState(false);
+  const [location, setLocation] = useState();
+  const [markerLocation, setMarkerLocation] = useState(undefined);
+
+  useEffect(() => {
+    if (!isPermission) requestPermission(setPermission);
+    Geolocation.getCurrentPosition(
+      position => {
+        const {latitude, longitude} = position.coords;
+        setLocation({latitude, longitude});
+        setMarkerLocation({latitude, longitude});
+      },
+      error => {
+        console.log(error.code, error.message);
+      },
+      {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+    );
+  }, [isPermission]);
+
   return (
     <View style={styles.mapScreen}>
       <View style={styles.mapTexts}>
@@ -24,13 +61,31 @@ const ScreenInfo = () => {
           justify={'center'}
         />
       </View>
-      {/* COLOCAR AQUI EL MAPA */}
+      {location !== undefined && markerLocation !== undefined ? (
+        <MapView
+          style={{width: '100%', height: '60%'}}
+          initialRegion={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          onRegionChangeComplete={({latitude, longitude}) => setMarkerLocation({latitude, longitude})}>
+          <Marker
+            title="Prueba"
+            coordinate={{
+              latitude: markerLocation.latitude,
+              longitude: markerLocation.longitude,
+            }}
+          />
+        </MapView>
+      ) : undefined}
       <GeneralButton title={'Asignar dirección'} />
     </View>
   );
 };
 
-export const MapDirection = navigation => {
+export const MapDirection = () => {
   return (
     <ContainerWhite>
       <Container>
