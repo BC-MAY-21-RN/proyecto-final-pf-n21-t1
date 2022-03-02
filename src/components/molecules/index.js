1f9bafa1bdaa53c7189030374573329eb627c4cc
adrianagh13 <gomezhinojosa.adriana@gmail.com>
2022-03-02 14:25:40 -0600

    Fix all conflicts due to rebase with master

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -14,8 +14,4 @@ export {ServicePicker} from './ServicePicker';
 export {TimePickers} from './TimePickers';
 export {ImageDetails} from './ImageDetails';
 export {AcceptedService} from './AcceptedService';
-<<<<<<< HEAD
-export {AcceptedService} from './AcceptedService';
-=======
->>>>>>> 0b2546d58d0e2a7a06421ffaba5c8c76c0db1cbb
 export {BackFinishBtns} from './BackFinishBtns';
