27b766e22bfe4151ba26dc2cb6d0a32ca61572dd
adrianagh13 <gomezhinojosa.adriana@gmail.com>
2022-03-02 18:51:06 -0600

    Buttons conflict solved
    Co-authored-by: Namoku <contact@namoku.xyz>

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -25,7 +25,7 @@ import {AuthProvider} from './AuthProvider';
 
 const Stack = createNativeStackNavigator();
 const stackNavigatorProps = {
-  initialRouteName: 'ServiceGrid',
+  initialRouteName: 'ProviderPreview',
   screenOptions: {
     headerShown: false,
   },
